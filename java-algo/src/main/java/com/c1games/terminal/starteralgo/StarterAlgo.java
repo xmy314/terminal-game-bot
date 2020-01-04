58987538ff430b38f5da2ef8a5cf90a109590632
xmy314 <44828636+xmy314@users.noreply.github.com>
2020-01-03 21:19:10 -0800

    start using git

--- java-algo/src/main/java/com/c1games/terminal/starteralgo/StarterAlgo.java
+++ java-algo/src/main/java/com/c1games/terminal/starteralgo/StarterAlgo.java
@@ -8,6 +8,7 @@ import com.c1games.terminal.algo.io.GameLoopDriver;
 import com.c1games.terminal.algo.map.GameState;
 import com.c1games.terminal.algo.map.MapBounds;
 import com.c1games.terminal.algo.units.UnitType;
+import com.c1games.terminal.algo.map.Unit;
 
 import java.util.*;
 
@@ -19,45 +20,68 @@ public class StarterAlgo implements GameLoop {
         new GameLoopDriver(new StarterAlgo()).run();
     }
 
-    private static final Coords[] firewallLocationsC = {
-            new Coords(8, 11),
-            new Coords(9, 11),
-            new Coords(7, 10),
-            new Coords(7, 9),
-            new Coords(7, 8),
-            new Coords(8, 7),
-            new Coords(9, 7)
+    private static final Coords[][] attacking_coords = {
+        {new Coords(9 , 4),new Coords(10, 3)},
+        {new Coords(18, 4),new Coords(17, 3)}
     };
 
-    private static final Coords[] firewallLocations1 = {
-            new Coords(17, 11),
-            new Coords(18, 11),
-            new Coords(18, 10),
-            new Coords(18, 9),
-            new Coords(18, 8),
-            new Coords(17, 7),
-            new Coords(18, 7),
-            new Coords(19, 7)
+    private static final Coords[] attacking_doors = {
+        new Coords(23, 10),
+        new Coords(4 ,10),
     };
 
-    private static final Coords[] firewallLocationsDots = {
-            new Coords(11, 7),
-            new Coords(13, 9),
-            new Coords(15, 11)
+    private static boolean attacking= false;
+
+    private static int choice_of_attack;
+
+    private static final int[][] directions ={
+        {0,1},
+        {1,1},
+        {1,0},
+        {1,-1},
+        {0,-1},
+        {-1,-1},
+        {-1,0},
+        {-1,1}
     };
 
-    private static final Coords[] defensiveDestructorLocations = {
-            new Coords(0, 13),
-            new Coords(27, 13)
+    private static int[][] activation_type = {
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 1, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 1, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 2, 1, 0, 0, 1, 2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+        { 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0},
+        { 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0},
+        { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1}
     };
 
-    private static final Coords[] defensiveEncryptorLocations = {
-            new Coords(3, 11),
-            new Coords(4, 11),
-            new Coords(5, 11)
+    private static int[][] activated={
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-8,-8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-8,-4,-4,-8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-7,-4,-7, 0,-4,-7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-6,-4, 0,-8, 0, 0,-4,-6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-7,-7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-7, 0, 0,-7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 3, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 0, 0, 0, 0},
+        { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+        { 0,-9, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,-9, 0},
+        { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1}
     };
 
     private final Random rand = new Random();
+
+
 
     @Override
     public void initialize(GameIO io, Config config) {
@@ -69,114 +93,119 @@ public class StarterAlgo implements GameLoop {
      */
     @Override
     public void onTurn(GameIO io, GameState move) {
-        io.debug().println("Performing turn " + move.data.turnInfo.turnNumber + " of your custom algo strategy");
-        buildC1Logo(move);
-        buildDefenses(move);
+        
+
+    
+
+
+        if (move.data.turnInfo.turnNumber==0){
+            /*
+                intializing the original setup 
+            */
+            for (int y=0 ; y<14 ; y++){
+                for (int x=0 ; x<28 ; x++){
+                    if (activated[y][x]>=1){
+                        Coords pos=new Coords(x,y);
+                        spawnBypos(move, pos);
+                    }
+                }
+            }
+        }else{
+            /* check every status 1 unit, 
+                if damaged or destroyed: rebuild and activate surrounding.
+            */
+            for (int y=13 ; y>=0 ; y-=1){
+                for (int x=0 ; x<28 ; x++){
+                    if (activated[y][x]>=1 && !( attacking && attacking_doors[choice_of_attack].x == x && attacking_doors[choice_of_attack].y == y) ){
+                        Coords pos=new Coords(x,y);
+                        Unit building = move.getWallAt(pos);
+                        if (! (building == null || building.stability<0.85)){
+                            continue;
+                        }
+                        if (building == null){
+                            spawnBypos(move, pos);
+                        }
+                        for (int[] dir : directions){
+                            Coords activiting_pos = new Coords(x+dir[0],y+dir[1]);
+                            if (activiting_pos.y<=13 && MapBounds.inArena(activiting_pos) && activation_type[y][x]!=0){
+                                activated[y][x]=1;
+                                spawnBypos(move, activiting_pos);
+                            }
+                        }
+                    }
+                }
+            }
+            /*
+            check remaining core and decide to activate next
+            */
+            if (move.data.p1Stats.cores>=20){
+                io.debug().println("let's expand with"+move.data.p1Stats.cores);
+
+                int aim=0;
+                for (int y=0 ; y<14 ; y++){
+                    for (int x=0 ; x<28 ; x++){
+                        if ( activated[y][x]<aim){
+                            aim=activated[y][x];
+                        }
+                    }
+                }
+
+                for (int y=0 ; y<14 ; y++){
+                    for (int x=0 ; x<28 ; x++){
+                        if (activated[y][x] == aim){
+                            activated[y][x]=1;
+                            io.debug().println("expanded at("+x+","+y+")");
+                            Coords pos=new Coords(x,y);
+                            spawnBypos(move,pos);
+                        }
+                        if (move.data.p1Stats.cores<5){
+                            break;
+                        }
+                    }
+
+                    if (move.data.p1Stats.cores<=5){
+                        break;
+                    }
+                }
+            }
+        }
+
+        /*
+            deploy information units
+        */
         deployAttackers(move);
     }
 
-    /**
-     * Make the C1 logo.
-     */
-    private void buildC1Logo(GameState move) {
-        move.attemptSpawnMultiple(Arrays.asList(firewallLocationsC), UnitType.Filter);
-        move.attemptSpawnMultiple(Arrays.asList(firewallLocations1), UnitType.Filter);
-        move.attemptSpawnMultiple(Arrays.asList(firewallLocationsDots), UnitType.Destructor);
-    }
-
-    /**
-     * Once the C1 logo is made, attempt to build some defenses.
-     */
-    private void buildDefenses(GameState move) {
-        /*
-        First lets protect ourselves a little with destructors.
-         */
-        move.attemptSpawnMultiple(Arrays.asList(defensiveDestructorLocations), UnitType.Destructor);
-
-        /*
-        Then lets boost our offense by building some encryptors to shield
-        our information units. Lets put them near the front because the
-        shields decay over time, so shields closer to the action
-        are more effective.
-         */
-        move.attemptSpawnMultiple(Arrays.asList(defensiveEncryptorLocations), UnitType.Encryptor);
-
-        /*
-        Lastly lets build encryptors in random locations. Normally building
-        randomly is a bad idea but we'll leave it to you to figure out better
-        strategies.
-
-        First we get all locations on the bottom half of the map
-        that are in the arena bounds.
-         */
-        List<Coords> possibleSpawnPoints = new ArrayList<>();
-        for (int x = 0; x < MapBounds.BOARD_SIZE; x++) {
-            for (int y = 0; y < MapBounds.BOARD_SIZE; y++) {
-                Coords c = new Coords(x, y);
-                if (move.canSpawn(c, UnitType.Encryptor, 1).affirmative())
-                    possibleSpawnPoints.add(c);
-            }
-        }
-        Collections.shuffle(possibleSpawnPoints);
-        while (move.numberAffordable(UnitType.Encryptor) > 0) {
-            Coords c = possibleSpawnPoints.remove(possibleSpawnPoints.size() - 1);
-            move.spawn(c, UnitType.Encryptor);
+    private void spawnBypos(GameState move, Coords pos){
+        if (activation_type[pos.y][pos.x]==1){
+            move.attemptSpawn(pos, UnitType.Filter);
+        } else if(activation_type[pos.y][pos.x]==2){
+            move.attemptSpawn(pos, UnitType.Destructor);
+        } else if(activation_type[pos.y][pos.x]==3){
+            move.attemptSpawn(pos, UnitType.Encryptor);
         }
     }
 
-    /**
-     * Deploy offensive units.
-     */
     private void deployAttackers(GameState move) {
-        /*
-        First lets check if we have 10 bits, if we don't we lets wait for
-        a turn where we do.
-         */
-        if (move.data.p1Stats.bits < 10)
+        double gate=0.04*move.data.turnInfo.turnNumber+12;
+        if (move.data.p1Stats.bits < gate && !attacking){
             return;
-
-        /*
-        Then lets deploy an EMP long range unit to destroy firewalls for us.
-         */
-        move.attemptSpawn(new Coords(3, 10), UnitType.EMP);
-
-        /*
-        Now lets send out 3 Pings to hopefully score, we can spawn multiple
-        information units in the same location.
-        */
-        for (int i = 0; i < 3; i++) {
-            move.attemptSpawn(new Coords(14, 0), UnitType.Ping);
+        }else if (attacking){
+            int p=0;
+            while (move.data.p1Stats.bits>=1){
+                move.attemptSpawn(attacking_coords[choice_of_attack][p], UnitType.Ping);
+                p=1-p;
+            }
+            attacking=false;
+        }else if (move.data.p1Stats.bits*0.75+5 >= gate){
+            choice_of_attack=rand.nextInt(2);
+            Unit door = move.getWallAt(attacking_doors[choice_of_attack]);
+            if (door!=null){
+                move.removeFirewall(attacking_doors[choice_of_attack]);
+            }
+            attacking=true;
         }
 
-        /*
-        NOTE: the locations we used above to spawn information units may become
-        blocked by our own firewalls. We'll leave it to you to fix that issue
-        yourselves.
-
-        Lastly lets send out Scramblers to help destroy enemy information units.
-        A complex algo would predict where the enemy is going to send units and
-        develop its strategy around that. But this algo is simple so lets just
-        send out scramblers in random locations and hope for the best.
-
-        Firstly information units can only deploy on our edges. So lets get a
-        list of those locations.
-         */
-        List<Coords> friendlyEdges = new ArrayList<>();
-        friendlyEdges.addAll(Arrays.asList(MapBounds.EDGE_LISTS[MapBounds.EDGE_BOTTOM_LEFT]));
-        friendlyEdges.addAll(Arrays.asList(MapBounds.EDGE_LISTS[MapBounds.EDGE_BOTTOM_RIGHT]));
-
-        /*
-        While we have remaining bits to spend lets send out scramblers randomly.
-        */
-        while (move.numberAffordable(UnitType.Scrambler) > 1) {
-            Coords c = friendlyEdges.get(rand.nextInt(friendlyEdges.size()));
-            move.attemptSpawn(c, UnitType.Scrambler);
-
-            /*
-            We don't have to remove the location since multiple information
-            units can occupy the same space.
-             */
-        }
     }
 
 }
